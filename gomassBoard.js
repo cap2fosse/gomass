9fd6bdc3071cd57af0fbed5bd4e7d47f150226e3
cap2fosse <cap2fosse@gmail.com>
2015-06-04 22:23:17 +0200

    0.08

--- gomassBoard.js
+++ gomassBoard.js
@@ -6,39 +6,80 @@ opponentBoard.selectedCarte = new Carte();
 opponentBoard.selectedCarte.init();
 opponentBoard.selectedCaseId = -1;
 opponentBoard.onclick = function() {
-  // received attack from player board
-  if (selectedCarte.visible && playerBoard.selectedCarte.visible) {
-    this.selectedCarte = selectedCarte.clone();
-    this.selectedCaseId = selectedCaseId
-    console.log(playerBoard.selectedCarte + '\n' + 'Attack ' + this.selectedCarte + '\n' + 
-        'From case : ' + playerBoard.selectedCaseId + '\n' + 'To case : ' + this.selectedCaseId);
-    // resolve attack here
-    
-    // end of attack
-    // reset
-    playerBoard.initSelectedCarte();
-    selectedCarte.init();
-    selectedCaseId = -1;
-  }
-  // received attack from player
-  else if (selectedCarte.visible && player.selectedCarte.visible) {
-    this.selectedCarte = selectedCarte.clone();
-    this.selectedCaseId = selectedCaseId
-    console.log('Player attack Opponent: ' + selectedCarte);
-    // revolve attack here
-    
-    // reset
-    player.initSelectedCarte();
-    selectedCarte.init();
-    selectedCaseId = -1;
+  if (selectedCarte.visible && myTurn) {
+    var onHand = cardOnHand();
+    switch (onHand) {
+      case 1: // received attack from player
+        this.selectedCarte = selectedCarte.clone();
+        this.selectedCaseId = selectedCaseId
+        console.log('Player attack Opponent: ' + selectedCarte);
+        // revolve attack here
+        
+        // reset
+        player.initSelectedCarte();
+        selectedCarte.init();
+        selectedCaseId = -1;
+        break;
+      case 2: // received attack from player hand
+        this.selectedCarte = selectedCarte.clone();
+        this.selectedCaseId = selectedCaseId
+        console.log('Player Hand attack Opponent: ' + selectedCarte);
+        playerHand.remove(playerHand.selectedCaseId);
+        // revolve attack here
+        
+        // reset
+        playerHand.initSelectedCarte();
+        selectedCarte.init();
+        selectedCaseId = -1;
+        break;
+      case 3: // received attack from player board
+        this.selectedCarte = selectedCarte.clone();
+        this.selectedCaseId = selectedCaseId
+        console.log('Player Board attack Opponent: ' + playerBoard.selectedCarte + '\n' + 'Attack ' + this.selectedCarte + '\n' + 
+            'From case : ' + playerBoard.selectedCaseId + '\n' + 'To case : ' + this.selectedCaseId);
+        // resolve attack here
+        
+        // end of attack
+        // reset
+        playerBoard.initSelectedCarte();
+        selectedCarte.init();
+        selectedCaseId = -1;
+        break;
+      default:
+        console.log("Can't select carte!");
+    }
   }
 }
 
 var playerBoard = new Board('playerBoard', 0, 250);
 playerBoard.create(8, 1);
 playerBoard.onclick = function() {
-  // first attack movement on board
-  if (selectedCarte.visible && myTurn && !this.selectedCarte.visible) {
+  var onHand = cardOnHand();
+  if (!selectedCarte.visible && myTurn) {
+    switch (onHand) {
+      case 1: // player do invocation on board
+        this.add(selectedCaseId, player.selectedCarte.clone());
+        console.log('Player invocation : ' + this.get(selectedCaseId));
+        // reset
+        player.initSelectedCarte();
+        selectedCarte.init();
+        selectedCaseId = -1;
+        break;
+      case 2: // playerHand drop a carte on the board
+        this.add(selectedCaseId, playerHand.selectedCarte);
+        playerHand.remove(playerHand.selectedCaseId);
+        console.log('Put a carte on board : ' + this.get(selectedCaseId));
+        // reset
+        playerHand.initSelectedCarte();
+        selectedCarte.init();
+        selectedCaseId = -1;
+        break;
+      default:
+        console.log("Can't select carte!");
+    }
+  }
+   // select a carte on board
+  else if (selectedCarte.visible && myTurn && onHand == 0) {
     this.selectedCarte = selectedCarte.clone();
     this.selectedCaseId = selectedCaseId
     console.log('Selected carte : ' + selectedCarte + ' at case id : ' + selectedCaseId);
@@ -46,64 +87,114 @@ playerBoard.onclick = function() {
     selectedCarte.init();
     selectedCaseId = -1;
   }
-  // playerHand drop a carte on the board
-  else if (!selectedCarte.visible && playerHand.selectedCarte.visible && !this.selectedCarte.visible) {
-    this.add(selectedCaseId, playerHand.selectedCarte);
-    console.log('Put a carte on board : ' + this.get(selectedCaseId));
-    // reset
-    playerHand.initSelectedCarte();
-    selectedCarte.init();
-    selectedCaseId = -1;
+  // unselect a carte on board
+  else if (selectedCarte.visible && selectedCarte.equal(this.selectedCarte)) {
+    this.initSelectedCarte();
+    console.log('Unselected carte : ' + selectedCarte);
   }
-  // player do invocation on board
-  else if (!selectedCarte.visible && player.selectedCarte.visible && !this.selectedCarte.visible) {
-    this.add(selectedCaseId, player.selectedCarte.clone());
-    console.log('Player invocation : ' + this.get(selectedCaseId));
-    // reset
-    player.initSelectedCarte();
-    selectedCarte.init();
-    selectedCaseId = -1;
-  }
-  // player stop attack
-  else if (selectedCarte.equal(this.selectedCarte)) {
-    this.selectedCarte.init();
-    console.log('Player stop attack : ' + selectedCarte);
+  else {
+    console.log("Can't select carte!");
   }
 }
 
 var playerHand = new Board('playerHandBoard', 0, 420);
 playerHand.create(8, 1);
 playerHand.onclick = function() {
-  if (selectedCarte.visible) {
-    this.selectedCarte = selectedCarte.clone();
-    this.selectedCaseId = selectedCaseId;
-    playerHand.remove(selectedCaseId);
-    selectedCarte.init();
-    selectedCaseId = -1;
-    console.log('Select a Carte in hand : ' + this.selectedCarte);
+  if (selectedCarte.visible && myTurn) {
+    var onHand = cardOnHand();
+    switch (onHand) {
+    case 0: // select a carte from hand
+      this.selectedCarte = selectedCarte.clone();
+      this.selectedCaseId = selectedCaseId;
+      
+      selectedCarte.init();
+      selectedCaseId = -1;
+      console.log('Select a Carte in Hand : ' + this.selectedCarte);
+      break;
+    default:
+      if (selectedCarte.equal(this.selectedCarte)) {
+        this.initSelectedCarte();
+        console.log('Unselected carte in Hand : ' + selectedCarte);
+      }
+    }
+  }
+  else {
+    console.log("Can't select carte!");
   }
 }
 
 var opponent = new Board('opponentBoard', 850, 140);
 opponent.create(1, 1);
 opponent.onclick = function() {
-  if (selectedCarte.visible && player.selectedCarte.visible) {
-      console.log('Player attack directe : ' + selectedCarte);
+  if (selectedCarte.visible && myTurn) {
+    var onHand = cardOnHand();
+    switch (onHand) {
+    case 1: // Player attack direct
+      console.log('Player attack direct : ' + selectedCarte);
+      this.selectedCarte = selectedCarte.clone();
+      this.selectedCaseId = selectedCaseId
+      // resolve attack here
+      console.log(player.selectedCarte + '\n' + 'Attack ' + this.selectedCarte + '\n' + 
+          'From case : ' + player.selectedCaseId + '\n' + 'To case : ' + this.selectedCaseId);
+      // reset
+      player.initSelectedCarte();
+      selectedCarte.init();
+      selectedCaseId = -1;
+      break;
+    case 2: // Hand attack direct
+      console.log('Hand attack direct : ' + selectedCarte);
+      this.selectedCarte = selectedCarte.clone();
+      this.selectedCaseId = selectedCaseId;
+      playerHand.remove(playerHand.selectedCaseId);
+      // resolve attack here
+      console.log(playerHand.selectedCarte + '\n' + 'Attack ' + this.selectedCarte + '\n' + 
+          'From case : ' + playerHand.selectedCaseId + '\n' + 'To case : ' + this.selectedCaseId);
+      // reset
+      playerHand.initSelectedCarte();
+      selectedCarte.init();
+      selectedCaseId = -1;
+      break;
+    case 3: // Board attack direct
+      console.log('Board attack direct : ' + selectedCarte);
+      this.selectedCarte = selectedCarte.clone();
+      this.selectedCaseId = selectedCaseId
+      // resolve attack here
+      console.log(playerBoard.selectedCarte + '\n' + 'Attack ' + this.selectedCarte + '\n' + 
+          'From case : ' + playerBoard.selectedCaseId + '\n' + 'To case : ' + this.selectedCaseId);
+      // reset
+      playerBoard.initSelectedCarte();
+      selectedCarte.init();
+      selectedCaseId = -1;
+      break;
+    default:
+      console.log("Can't select carte!");
+    }
   }
-  player.initSelectedCarte();
+}
+
+var player = new Board('player', 850, 320);
+player.create(1, 1);
+player.onclick = function() {
+  if (selectedCarte.visible && myTurn) {
+    var onHand = cardOnHand();
+    switch (onHand) {
+    case 0: // select a carte from player
+      this.selectedCarte = selectedCarte.clone();
+      console.log('Selected player : ' + selectedCarte);
+      break;
+    default:
+      console.log("Can't select carte!");
+    }
+  }
   selectedCarte.init();
   selectedCaseId = -1;
 }
-
-var player = new Board('playerBoard', 850, 320);
-player.create(1, 1);
-player.onclick = function() {
-  if (selectedCarte.visible) {
-    this.selectedCarte = selectedCarte.clone();
-    console.log('Selected Carte in playerBoard : ' + selectedCarte);
-  }
-  selectedCarte.init();
-  selectedCaseId = -1;
+// return 0 if no card on hand
+function cardOnHand() {
+  if (player.selectedCarte.visible) return 1;
+  else if (playerHand.selectedCarte.visible) return 2;
+  else if (playerBoard.selectedCarte.visible) return 3;
+  else return 0;
 }
 // insert some demo cards
 playerHand.add(0, new Carte(0, "images/carte6.jpg", "10", "10", "10", "capitaine", "012345678901234", true, false));
---
+++ gomassClt.js
@@ -0,0 +1,132 @@
+// URL
+var rootUrl = 'http://localhost:3000';
+// socket creation
+var socket = io.connect(rootUrl);
+
+//BEGIN CONNECTION
+socket.on('login', function(message) {
+  if (message.accepted) {
+    console.log('Received login : you are accepted : ' + message.player);
+  }
+  else {
+    console.log('Received login : login already exist : ' + message.player);
+    userNameB.disabled = false;
+    userNameT.disabled = false;
+  }
+})
+socket.on('userjoined', function(message) {
+  if (message.accepted) {
+    console.log('Received user joined : ' + message.player);
+  }
+})
+socket.on('userleft', function(message) {
+  //gomassChat.value += message.username + ' : leave the game!' + '\n';
+  console.log('Received user left : ' + message.username + ' : ' + message.message);
+})
+//END CONNECTION
+//-----------------------------------------------------------
+
+//BEGIN CHAT
+socket.on('newmessageok', function(message) {
+  gomassChat.value += message.username + ' : ' + message.message + '\n';
+  sendT.value = '';
+})
+//END CHAT
+//-----------------------------------------------------------
+
+//BEGIN CREATE GAME
+socket.on('newgameok', function(message) {
+  if (message.accepted) {
+    gameName = message.game;
+    disableCommands(true);
+    console.log('Received newgameok : ' + message.validated);
+  }
+  else {
+    console.log('Received newgameok : ' + message.validated);
+  }
+})
+socket.on('joingameok', function(message) {
+  gameName = message.game;
+  createT.value = gameName;
+  disableCommands(true);
+  console.log('Received joingameok : ' + message.validated);
+})
+socket.on('opengame', function(message) {
+  gomassListGame.addOpt(message.game);
+  console.log('Received openGame : ' + message.validated);
+})
+socket.on('closegame', function(message) {
+  // remove the game from the list
+  gomassListGame.remove(message.index);
+  console.log('Received closeGame : ' + message.validated);
+})
+//END CREATE GAME
+//-----------------------------------------------------------
+
+//BEGIN GAME
+socket.on('startgame', function(message) {
+  console.log('Received startgame : ' + message.validated);
+  // remove the game from the list
+  gomassListGame.remove(message.index);
+  // who is opponent ?
+  if (creator) opponentName = message.player2;
+  else opponentName = message.player1;
+  // show the game
+  showGame(true);
+  // who play in first ?
+  if (message.first == playerName) {
+    myTurn = true;
+    endTurnB.disabled = false;
+    console.log('You start the game : ' + message.game);
+  }
+  else {
+    myTurn = false;
+    endTurnB.disabled = true;
+    console.log('You wait your turn : ' + message.game);
+  }
+})
+socket.on('mymove', function(message) {
+  console.log('Received mymove : ' + message.validated);
+})
+socket.on('hismove', function(message) {
+  console.log('Received hismove : ' + message.validated);
+  var tempMove = message.move.split(",");
+  console.log('move : '+ tempMove[0] +' : '+ tempMove[1]);
+  mainBoard.move(tempMove[0], tempMove[1]);
+})
+socket.on('endturnok', function(message) {
+  console.log('Received endturnok : ' + message.validated);
+  // my turn?
+  if (message.first) {
+    myTurn = true;
+    endTurnB.disabled = false;
+    // ask is it the end of game
+    socket.emit('endgame', {
+      name: gameName,
+      player: playerName
+    });
+  }
+  else {
+    myTurn = false;
+    endTurnB.disabled = true;
+  }
+})
+socket.on('endgameok', function(message) {
+  console.log('Received endgameok : ' + message.validated);
+  // win the game?
+  if (message.win) {
+    console.log('You win the game : ' + message.game);
+    myTurn = false;
+    endTurnB.disabled = true;
+  }
+  else {
+    console.log('You loose the game : ' + message.game);
+    myTurn = false;
+    endTurnB.disabled = true;
+  }
+  // go to create a new game
+  disableCommands(false);
+  initGlobal();
+  showGame(false);
+})
+//END GAME
